db7bda7de678842ab5b517d691ab15235291b303
Adam Szecsi-Tisza <adam@Adams-MacBook-Pro.local>
2021-09-19 17:05:37 +0200

    Edit homepage instruction

--- src/components/instructions/instructions.data.js
+++ src/components/instructions/instructions.data.js
@@ -1,6 +1,6 @@
 const instructionsData = {
     'about-page': 'about page something',
-    'home-page': 'home page something',
+    'home-page': 'home page instruction',
     'record-page': 'record page something',
 }
 
